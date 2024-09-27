aad9cf57624c54c82946c92261a3daa20c12cf31
R15pawan <ramamandhana935@gmail.com>
2024-09-27 20:05:28 +0530

    Add files via upload

---
+++ Project_Grea.py.py
@@ -0,0 +1,10 @@
+# Variables to store sentences
+name_sentence = "My name is Student."
+age_sentence = " I am 10 years old."
+grade_sentence = " I am in grade 6."
+
+# Concatenating the sentences
+full_sentence = name_sentence + age_sentence + grade_sentence
+
+# Printing the concatenated result
+print(full_sentence)
